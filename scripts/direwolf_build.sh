d2289216681e76e618b9010bc195f8679c146470
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-03-01 00:43:11 -0600

    Update direwolf_build.sh

--- scripts/direwolf_build.sh
+++ scripts/direwolf_build.sh
@@ -1,6 +1,6 @@
 echo 'Building Direwolf'
 cd ~/build
-sudo apt-get install git gcc g++ make cmake libasound2-dev libudev-dev -y
+sudo apt-get install git gcc g++ make cmake libasound2-dev libudev-dev libgpsd-dev -y
 git clone https://www.github.com/wb2osz/direwolf
 cd direwolf
 git branch -r
