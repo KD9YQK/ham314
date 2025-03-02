cc500afb97c3cd5f8c2b18dceb887a6208d28917
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-03-01 18:44:40 -0600

    Update ax25stop.sh

--- launchers/ax25stop.sh
+++ launchers/ax25stop.sh
@@ -1,6 +1,14 @@
+echo 'Backing up node'
 sudo nodesave /etc/ax25/nodebackup.sh
+echo 'Closing mheardd'
 sudo killall mheardd
+echo 'Closing ax25d'
 sudo killall ax25d
+echo 'closing kissattach'
 sudo killall kissattach
+echo 'Closing direwolf'
 sudo killall direwolf
-
+echo 'Closing netromd'
+sudo killall netromd
+ifconfig nr0 down
+echo 'Shutdown complete'
