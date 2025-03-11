2ac69f90929fd8cf5951f0e2b201d21c06728135
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-03-10 20:22:24 -0500

    Update ax25_install.sh

--- scripts/ax25_install.sh
+++ scripts/ax25_install.sh
@@ -1,12 +1,12 @@
 echo "Install AX.25 Packet Node"
 sleep 1
 sudo apt install gpsd libgps28 -y
-sudo dpkg -i ../deb/direwolf*.deb
-sudo dpkg -i ../deb/libax25_1*.deb
-sudo dpkg -i ../deb/libax25-dev*.deb
-sudo dpkg -i ../deb/ax25apps*.deb
-sudo dpkg -i ../deb/ax25tools*.deb
-sudo dpkg -i ../deb/uronode_*.deb
+#sudo dpkg -i ../deb/direwolf*.deb
+#sudo dpkg -i ../deb/libax25_1*.deb
+#sudo dpkg -i ../deb/libax25-dev*.deb
+#sudo dpkg -i ../deb/ax25apps*.deb
+#sudo dpkg -i ../deb/ax25tools*.deb
+#sudo dpkg -i ../deb/uronode_*.deb
 #sudo apt-get install ax25-tools ax25-apps telnet ax25-node -y
 #sudo apt install uronode openbsd-inetd tcpd -y
 sudo apt install telnet openbsd-inetd tcpd -y
