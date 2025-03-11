0112c6fb7920d9c4b39cb3225c3d4578ac3430f6
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-03-10 19:58:53 -0500

    Update ardop_install.sh

--- scripts/ardop_install.sh
+++ scripts/ardop_install.sh
@@ -1,11 +1,7 @@
-# Download and install ardopc (URL is for the Raspberry Pi version)
-wget 'http://www.cantab.net/users/john.wiseman/Downloads/Beta/piardopc64' -O /tmp/ardopc
-sudo mv /tmp/ardopc /usr/local/bin/
+sudo CP apps/ardop/ardopc /usr/local/bin/
 sudo chmod +x /usr/local/bin/ardopc
+
 # Find the correct sound card identifier (See video tutorial and John Wiseman's comments)
-aplay -l
-
-
-
+#aplay -l
 # Configure sound devices (You may need to change "plughw:1,0" depending on above aplay output)
-echo 'pcm.ARDOP {type rate slave {pcm "plughw:3,0" rate 48000}}' > ~/.asoundrc
+#echo 'pcm.ARDOP {type rate slave {pcm "plughw:3,0" rate 48000}}' > ~/.asoundrc
