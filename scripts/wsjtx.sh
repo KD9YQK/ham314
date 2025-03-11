a84a12b894d72b44cf73e8ceda2d472705ee21a2
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-03-10 20:30:50 -0500

    Update wsjtx.sh

--- scripts/wsjtx.sh
+++ scripts/wsjtx.sh
@@ -1,11 +1,11 @@
 echo 'Install WSJT-X'
 sleep 1
-sudo apt install wsjtx -y
+#sudo apt install wsjtx -y
 #cd ~/build
-#sudo apt install libgfortran5 libqt5widgets5 libqt5network5 \
-#     libqt5printsupport5 libqt5multimedia5-plugins libqt5serialport5 \
-#     libqt5sql5-sqlite libfftw3-single3 libgomp1 libboost-all-dev \
-#     libusb-1.0-0 libportaudio2 -y
-#wget https://sourceforge.net/projects/wsjt/files/wsjtx-2.6.1/wsjtx_2.6.1_armhf.deb
-#sudo dpkg -i wsjtx_2.6.1_armhf.deb
-#sudo apt --fix-broken install -y
+sudo apt install libgfortran5 libqt5widgets5 libqt5network5 \
+     libqt5printsupport5 libqt5multimedia5-plugins libqt5serialport5 \
+     libqt5sql5-sqlite libfftw3-single3 libgomp1 libboost-all-dev \
+     libusb-1.0-0 libportaudio2 -y
+wget https://sourceforge.net/projects/wsjt/files/wsjtx-2.7.0/wsjtx_2.7.0_armhf.deb
+sudo dpkg -i wsjtx_2.7.0_armhf.deb -y
+sudo apt --fix-broken install -y
