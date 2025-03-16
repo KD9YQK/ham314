5f327365ba68dcbf3d16afd0c0b622834d7da025
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-03-15 22:52:04 -0500

    Update chirp.sh

--- scripts/chirp.sh
+++ scripts/chirp.sh
@@ -1,5 +1,5 @@
 echo 'Install Chirp'
 cd ~/build
-sudo apt install git python3-wxgtk4.0 python3-serial python3-six python3-future python3-requests python3-pip -y
-wget https://archive.chirpmyradio.com/chirp_next/next-20240312/chirp-20240312-py3-none-any.whl
-pip install ./chirp-20240312-py3-none-any.whl
+sudo apt install git python3-wxgtk4.0 python3-serial python3-six python3-future python3-requests python3-pip pipx -y
+wget https://archive.chirpmyradio.com/chirp_next/next-20250314/chirp-20250314-py3-none-any.whl
+pipx install --system-site-packages ./chirp-20250314-py3-none-any.whl
