a4a97a92548971e7113de3e4675f98610b4bb4df
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-03-15 22:37:51 -0500

    Update pcsi.sh

--- scripts/pcsi.sh
+++ scripts/pcsi.sh
@@ -1,7 +1,8 @@
 echo 'Build PCSI'
+sudo apt-get install libopenblas-dev
 cd ~
 git clone https://github.com/maqifrnswa/PCSI.git
-python -m venv PCSI/
+python -m venv --system-site-packages PCSI/
 #sudo apt install python3-opencv python3-tk python3-pil.imagetk -y
 sudo apt install python3-opencv
 PCSI/bin/pip install numpy==2.0.2 imageio bitstring==4.1.4 pylbfgs pyserial pillow
