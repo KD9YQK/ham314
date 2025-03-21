51a42a506299556c4e1965da186136e980425209
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-03-21 17:04:30 -0500

    Update conky-start.sh

--- conky/conky-start.sh
+++ conky/conky-start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 killall conky
-cd /home/kd9yqk/.config/conky/
-/usr/local/bin/conky -c system.rc 2> /dev/null
-/usr/local/bin/conky -c radio.rc
-/usr/local/bin/conky -c location.rc
-/usr/local/bin/conky -c processes.rc
-/usr/local/bin/conky -c direwolf.rc
-/usr/local/bin/conky -c pat.rc
+cd /home/kd9yqk/ham314/conky/
+conky -c system.rc 2> /dev/null
+conky -c radio.rc
+conky -c location.rc
+conky -c processes.rc
+conky -c direwolf.rc
+conky -c pat.rc
