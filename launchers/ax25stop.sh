637a9a377e21c43a299b0d0fa99f04c9d94b15a7
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-03-21 20:22:55 -0500

    Update ax25stop.sh

--- launchers/ax25stop.sh
+++ launchers/ax25stop.sh
@@ -8,7 +8,4 @@ echo 'closing kissattach'
 sudo killall kissattach
 echo 'Closing direwolf'
 sudo killall direwolf
-echo 'Closing netromd'
-sudo killall netromd
-ifconfig nr0 down
 echo 'Shutdown complete'
