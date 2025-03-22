4798ec4ae96763292b34129152478731361a0aab
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-03-21 20:28:00 -0500

    Update ax25service.sh

--- launchers/ax25service.sh
+++ launchers/ax25service.sh
@@ -19,22 +19,31 @@ sudo killall direwolf
 # zero out old direwolf log file in case /tmp/ is full
 truncate --size 0 /tmp/direwolf.log
 
+#echo "Starting direwolf"
 sudo -u kd9yqk direwolf -t 0 -c /home/kd9yqk/direwolf-node.conf -p > /tmp/direwolf.log &
 
+#Check if Direwolf is running
+#echo "Sleeping 5 sec."
 sleep 5
+if [ -z "`ps ax | grep -v grep | grep direwolf`" ]; then
+  #echo -e "\nERROR: Direwolf did not start properly and is not running, please review direwolf.conf"
+  exit 1
+fi
+
+sudo modprobe netrom
 nrdevice=`ifconfig | grep nr0 | wc -l`
 if [ $nrdevice -eq 0 ]; then
-   sudo modprobe netrom
-   sudo nrattach netrom  # run this once per boot
+  sudo nrattach netrom  # run this once per boot
 fi
 
 sudo kissattach `ls -l /tmp/kisstnc | awk '{ print $11 }'` ax0
 
 sudo kissparms -c 1 -p ax0  # fix invalid port first to tries on direwolf
-sudo ax25d
-sudo /etc/ax25/nodebackup.sh
-sudo /usr/sbin/netromd
+sudo ax25d  # for rmsgw only
+sudo /etc/ax25/nodebackup.sh  # restore node table and routes we heard last time
+sudo /usr/sbin/netromd        # Start the netrom service, lists for nodes/routes
 sudo mheardd
+#sudo route del -net 44.0.0.0 netmask 255.0.0.0  # kill tcp traffic to ax0, updatesysop.py hits api.winlink.org on net 44
 
 # backup node data hourly
 while true; do
