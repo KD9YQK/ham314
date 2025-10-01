0d2e3a04086b71e42af4d596c99c13d4d0c5a421
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-10-01 14:44:28 -0500

    Update js8call.sh

--- scripts/js8call.sh
+++ scripts/js8call.sh
@@ -1,6 +1,12 @@
-echo 'Install js8call 2.2.0'
+echo 'Build js8call 2.2.0'
 sleep 1
-sudo apt install js8call -y
-#cd ~/build
-#wget http://files.js8call.com/2.2.0/js8call_2.2.0_armhf.deb
-#sudo dpkg -i js8call_2.2.0_armhf.deb
+#sudo apt install js8call -y
+cd ~/build
+sudo apt-get build-dep js8call
+sudo apt remove libhamlib4 # Required to use compiled hamlib
+wget http://files.js8call.com/2.2.0/js8call-2.2.0.tgz
+tar -zxvf js8call-2.2.0.tgz
+cd js8call
+cmake ~/build/js8call
+make -j 4
+sudo make install
