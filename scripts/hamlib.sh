e4cac62111134235517138ac82e69723cd62508c
KD9YQK <165826906+KD9YQK@users.noreply.github.com>
2025-10-01 14:23:28 -0500

    Update hamlib.sh

--- scripts/hamlib.sh
+++ scripts/hamlib.sh
@@ -1,14 +1,15 @@
-echo 'Building Hamlib 4.6.2'
+echo 'Building Hamlib 4.6.5'
 cd ~/build
 sudo apt-get update
 sudo apt-get install libfltk1.3-dev libjpeg9-dev libxft-dev libxinerama-dev libxcursor-dev libsndfile1-dev libsamplerate0-dev portaudio19-dev libusb-1.0-0-dev libpulse-dev libudev-dev texinfo -y
 sudo apt-get install -y libjpeg62-turbo-dev
-wget https://github.com/Hamlib/Hamlib/releases/download/4.6.2/hamlib-4.6.2.tar.gz
-tar -zxvf hamlib-4.6.2.tar.gz
-cd hamlib-4.6.2
-./configure --disable-shared --prefix="/usr/local" --without-cxx-binding \
-    --disable-winradio CFLAGS="-fdata-sections -ffunction-sections" \
-    LDFLAGS="-Wl,--gc-sections" LIBUSB_LIBS="/usr/lib/arm-linux-gnueabihf/libusb-1.0.a"
+wget https://github.com/Hamlib/Hamlib/releases/download/4.6.5/hamlib-4.6.5.tar.gz
+tar -zxvf hamlib-4.6.5.tar.gz
+cd hamlib-4.6.5
+./configure
+#./configure --disable-shared --prefix="/usr/local" --without-cxx-binding \
+#    --disable-winradio CFLAGS="-fdata-sections -ffunction-sections" \
+#    LDFLAGS="-Wl,--gc-sections" LIBUSB_LIBS="/usr/lib/arm-linux-gnueabihf/libusb-1.0.a"
 echo "Configure complete, Make in 5 seconds"
 sleep 5
 make -j 4
